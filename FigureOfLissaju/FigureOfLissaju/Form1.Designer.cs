856fb9f235df127a097089e2877e0423ed9b8752
Grigosty <122815869+Grigosty@users.noreply.github.com>
2023-04-11 19:25:14 +0300

    Add files via upload
    Проект с "Фигурами Лиссажу"

---
+++ FigureOfLissaju/FigureOfLissaju/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace FigureOfLissaju
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(20, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(850, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(876, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Построить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(876, 417);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 31);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Стоп";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(876, 80);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(876, 124);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(876, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Частота";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(876, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Сдвиг";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(446, 403);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(259, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Для черной функции: x = Cos(t), y = Sin(t)";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(446, 419);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(308, 32);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Выберите зависимости для красной функции.\r\nЛибо посмотрите настройки по-умолчанию" +
+    "";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "sin",
+            "cos",
+            "tan"});
+            this.checkedListBox1.Location = new System.Drawing.Point(876, 180);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(140, 55);
+            this.checkedListBox1.TabIndex = 15;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Items.AddRange(new object[] {
+            "sin",
+            "cos",
+            "tan"});
+            this.checkedListBox2.Location = new System.Drawing.Point(876, 272);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(143, 55);
+            this.checkedListBox2.TabIndex = 16;
+            this.checkedListBox2.SelectedIndexChanged += new System.EventHandler(this.checkedListBox2_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(878, 161);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(141, 16);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Зависимость для Х(t)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(878, 253);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(142, 16);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Зависимость для Y(t)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(876, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Амплитуда";
+            this.label1.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(876, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Visible = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(876, 376);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(144, 35);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Очистить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1031, 460);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ FigureOfLissaju/FigureOfLissaju/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FigureOfLissaju
+{
+    
+    public partial class Form1 : Form
+    {
+        public static double X = 0;
+        public static double Y = 0; 
+        public int A = 5;
+        public int W = 1;
+        public double fi0 = 0;
+        
+        public void Form1_Paint(object sender, PaintEventArgs e)
+        {
+           
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = 10;
+            
+            try
+            {
+                W = Convert.ToInt32(textBox1.Text);
+            }
+            catch
+            {
+                W = 1;
+            }
+            try
+            {
+                fi0 = Convert.ToInt32(textBox1.Text);
+            }
+            catch
+            {
+                fi0 = 0;
+            }
+
+
+           
+            if (timer1.Enabled == true)
+            {
+                timer1.Stop();
+                timer1.Start();
+            }
+            else
+            {
+                timer1.Start();
+            }
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            
+
+            Graphics g = pictureBox1.CreateGraphics();
+            g.TranslateTransform(pictureBox1.Width / 2, pictureBox1.Height / 2);
+            g.ScaleTransform(1, -1);
+            g.DrawLine(new Pen(Color.Black, 1), (float)(10 *A * Math.Cos(W * X))/* x1 */, (float)(10 *  A * Math.Sin(W * X))/* y1 */, (float)(10 * A * Math.Cos(W * X + 0.5))/* x2 */, (float)(10 * A * Math.Sin(W * X + 0.5))/* y2 */);
+            switch (checkedListBox1.SelectedItem)
+            {
+                case "sin":
+                    switch (checkedListBox2.SelectedItem)
+                    {
+                        case "sin":
+                            g.DrawLine(new Pen(Color.Red, 10), (float)(10 * A * Math.Sin(W * Y + fi0))/* x1 */, (float)(10 * A * Math.Sin(W * Y + fi0))/* y1 */, (float)(10 * A * Math.Sin(W * Y + fi0) + 0.5)/* x2 */, (float)(10 * A * Math.Sin(W * Y + fi0) + 0.05)/* y2 */);
+                            break;
+                        case "cos":
+                            g.DrawLine(new Pen(Color.Red, 10), (float)(10 * A * Math.Sin(W * Y + fi0))/* x1 */, (float)(10 * A * Math.Cos(W * Y + fi0))/* y1 */, (float)(10 * A * Math.Sin(W * Y + fi0) + 0.5)/* x2 */, (float)(10 * A * Math.Cos(W * Y + fi0) + 0.05)/* y2 */);
+                            break;
+                        case "tan":
+                            g.DrawLine(new Pen(Color.Red, 10), (float)(10 * A * Math.Sin(W * Y + fi0))/* x1 */, (float)(10 * A * Math.Tan(W * Y + fi0))/* y1 */, (float)(10 * A * Math.Sin(W * Y + fi0) + 0.5)/* x2 */, (float)(10 * A * Math.Tan(W * Y + fi0) + 0.05)/* y2 */);
+                            break;
+                        default:
+                            g.DrawLine(new Pen(Color.Red, 10), (float)(10 * A * Math.Sin(W * Y + fi0))/* x1 */, (float)(10 * A * Math.Sin(W * Y + fi0))/* y1 */, (float)(10 * A * Math.Sin(W * Y + fi0) + 0.5)/* x2 */, (float)(10 * A * Math.Sin(W * Y + fi0) + 0.05)/* y2 */);
+                            break;
+                    }
+                    break;
+
+                case "cos":
+                    switch (checkedListBox2.SelectedItem)
+                    {
+                        case "sin":
+                            g.DrawLine(new Pen(Color.Red, 10), (float)(10 * A * Math.Cos(W * Y + fi0))/* x1 */, (float)(10 * A * Math.Sin(W * Y + fi0))/* y1 */, (float)(10 * A * Math.Cos(W * Y + fi0) + 0.5)/* x2 */, (float)(10 * A * Math.Sin(W * Y + fi0) + 0.05)/* y2 */);
+                            break;
+                        case "cos":
+                            g.DrawLine(new Pen(Color.Red, 10), (float)(10 * A * Math.Cos(W * Y + fi0))/* x1 */, (float)(10 * A * Math.Cos(W * Y + fi0))/* y1 */, (float)(10 * A * Math.Cos(W * Y + fi0) + 0.5)/* x2 */, (float)(10 * A * Math.Cos(W * Y + fi0) + 0.05)/* y2 */);
+                            break;
+                        case "tan":
+                            g.DrawLine(new Pen(Color.Red, 10), (float)(10 * A * Math.Cos(W * Y + fi0))/* x1 */, (float)(10 * A * Math.Tan(W * Y + fi0))/* y1 */, (float)(10 * A * Math.Cos(W * Y + fi0) + 0.5)/* x2 */, (float)(10 * A * Math.Tan(W * Y + fi0) + 0.05)/* y2 */);
+                            break;
+                        default:
+                            g.DrawLine(new Pen(Color.Red, 10), (float)(10 * A * Math.Cos(W * Y + fi0))/* x1 */, (float)(10 * A * Math.Cos(W * Y + fi0))/* y1 */, (float)(10 * A * Math.Cos(W * Y + fi0) + 0.5)/* x2 */, (float)(10 * A * Math.Cos(W * Y + fi0) + 0.05)/* y2 */);
+                            break;
+                    }
+                    break;
+
+                case "tan":
+                    switch (checkedListBox2.SelectedItem)
+                    {
+                        case "sin":
+                            g.DrawLine(new Pen(Color.Red, 10), (float)(10 * A * Math.Tan(W * Y + fi0))/* x1 */, (float)(10 * A * Math.Sin(W * Y + fi0))/* y1 */, (float)(10 * A * Math.Tan(W * Y + fi0) + 0.5)/* x2 */, (float)(10 * A * Math.Sin(W * Y + fi0) + 0.05)/* y2 */);
+                            break;
+                        case "cos":
+                            g.DrawLine(new Pen(Color.Red, 10), (float)(10 * A * Math.Tan(W * Y + fi0))/* x1 */, (float)(10 * A * Math.Cos(W * Y + fi0))/* y1 */, (float)(10 * A * Math.Tan(W * Y + fi0) + 0.5)/* x2 */, (float)(10 * A * Math.Cos(W * Y + fi0) + 0.05)/* y2 */);
+                            break;
+                        case "tan":
+                            g.DrawLine(new Pen(Color.Red, 10), (float)(10 * A * Math.Tan(W * Y + fi0))/* x1 */, (float)(10 * A * Math.Tan(W * Y + fi0))/* y1 */, (float)(10 * A * Math.Tan(W * Y + fi0) + 0.5)/* x2 */, (float)(10 * A * Math.Tan(W * Y + fi0) + 0.05)/* y2 */);
+                            break;
+                        default:
+                            g.DrawLine(new Pen(Color.Red, 10), (float)(10 * A * Math.Tan(W * Y + fi0))/* x1 */, (float)(10 * A * Math.Tan(W * Y + fi0))/* y1 */, (float)(10 * A * Math.Tan(W * Y + fi0) + 0.5)/* x2 */, (float)(10 * A * Math.Tan(W * Y + fi0) + 0.05)/* y2 */);
+                            break;
+                    }
+                    break;
+
+                default: 
+                    break;
+            }
+          //  g.DrawLine(new Pen(Color.Red, 10), (float)(10 * A * Math.Cos(W * Y + fi0) )/* x1 */, (float)(10 * A * Math.Sin(W * Y + fi0) )/* y1 */, (float)(10 * A * Math.Cos(W * Y + fi0)  + 1)/* x2 */, (float)(10 * A * Math.Sin(W * Y + fi0)  + 1)/* y2 */);
+            X += 0.05;
+            Y += 0.05;
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(SystemColors.ActiveCaption);
+        }
+    }
+}
