6549b78da618beeada0887719f705c68007bd880
Grigosty <122815869+Grigosty@users.noreply.github.com>
2024-04-12 21:59:31 +0300

    Delete Задание SQL directory

--- Задание SQL/Запрос 3.sql
+++
@@ -1,6 +0,0 @@
-
---привязываем 
-INSERT INTO Good_cat
-SELECT Goods.good_ID, Categories.category_ID
-FROM Goods
-	join Categories on Categories.category_ID = Goods.category_ID
--- Задание SQL/Запрос 4.sql
+++
@@ -1,6 +0,0 @@
-
-
-SELECT good, category
-from Goods g
-LEFT JOIN Categories c on Good_cat.category_ID = c.category
---при помощи данного запроса мы выводим поля good и category 
